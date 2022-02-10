3d41429208ea5c0c929b3dfab09c55b6ca400bc6
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-02-09 16:34:04 -0800

    some more light css

--- src/components/GuestEntry/GuestEntry.jsx
+++ src/components/GuestEntry/GuestEntry.jsx
@@ -2,21 +2,28 @@ import React from 'react';
 import style from './GuestEntry.css';
 import { deleteEntryById } from '../../services/fetch';
 import { useEntires } from '../../context/EntriesContext';
+import { useToggle } from '../../context/ToggleContext';
 
 export default function ({ name, entry, id }) {
   const { setLoading } = useEntires();
+  const { darkMode } = useToggle();
   const handleDelete = (id) => {
     deleteEntryById(id);
     setLoading(true);
   };
 
   return (
-    <>
+    <div className={style.cardContainer}>
       <div className={style.guestEntry}>
         <h3>{entry}</h3>
         <h6>-{name}</h6>
-        <button onClick={() => handleDelete(id)}>delete</button>
       </div>
-    </>
+      <button
+        onClick={() => handleDelete(id)}
+        className={darkMode ? style.darkmode : null}
+      >
+        delete
+      </button>
+    </div>
   );
 }
