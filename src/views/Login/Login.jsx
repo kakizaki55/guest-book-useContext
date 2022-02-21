1f969ae5ddb8e8c375738f3a0d5ce939c8e32f64
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-02-21 14:44:32 -0800

    handle log in button working" "

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
 
 export default function Login() {
-  return <div>Login</div>;
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log('handle login insde');
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          handleLogin(e);
+        }}
+      >
+        <label>
+          Email :
+          <input name="email" type="email" />
+        </label>
+        <label>
+          Password :
+          <input name="password" type="password" />
+        </label>
+        <button aria-label="login">Login</button>
+      </form>
+    </div>
+  );
 }
