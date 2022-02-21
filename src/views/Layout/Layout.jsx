2535e444bb436f5cf200b211e6e34cae082e673c
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-02-21 14:32:24 -0800

    got my two routes set up

--- src/views/Layout/Layout.jsx
+++ src/views/Layout/Layout.jsx
@@ -3,13 +3,24 @@ import Home from '../Home/Home';
 import Header from '../../components/Layout/Header';
 import { useToggle } from '../../context/ToggleContext';
 import style from './Layout.css';
+import Login from '../Login/Login';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 export default function Layout() {
   const { darkMode } = useToggle();
   return (
     <div className={darkMode ? style.darkmode : null}>
       <Header />
-      <Home />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
       <footer>Hope you had a good week!</footer>
     </div>
   );
