a22e14a2edb5bcce0addc2f09eea55dfab1f5044
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-02-22 10:05:18 -0800

    trying to get the build working

--- src/services/client.jsx
+++ src/services/client.jsx
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-import { subbase } from '../utils/settings';
+
+console.log('LOOK HERER~!', process.env.REACT_APP_SUPABASE_URL);
 export const client = createClient(
   process.env.REACT_APP_SUPABASE_URL,
   process.env.REACT_APP_SUPABASE_KEY
